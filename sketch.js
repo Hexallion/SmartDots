7757f8528d19e2caf93d14aac5169fc16df3f620
Hexallion <39310789+Hexallion@users.noreply.github.com>
2018-12-30 23:38:29 +0000

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+/*
+SmartDots Algorithm - By Peter Cresswell
+
+sketch
+Central script controlling the biome
+*/
+
+function setup() {
+	settings = new Settings();
+	createCanvas(700, 700);
+}
+
+function draw() {
+	background(255, 255, 0)
+}
