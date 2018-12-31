0159b60dcd62360766dd9f85f0873a67c6f81f46
Peter <peter.cresswell7@gmail.com>
2018-12-31 00:14:02 +0000

    Created a Dot object Allowed position to be parsed to it.

---
+++ Dot.js
@@ -0,0 +1,10 @@
+/*
+SmartDots Algorithm - By Peter Cresswell
+
+Dot
+Dot Object, The object that learns
+*/
+
+function Dot(startx, starty){
+  this.PVector = createVector(startx, starty);
+}
--- sketch.js
+++ sketch.js
@@ -7,9 +7,12 @@ Central script controlling the sketch
 
 function setup() {
 	settings = new Settings();
+	dot = new Dot(settings.WinWidth/2, settings.WinHeight - 100);
 	createCanvas(settings.WinWidth, settings.WinHeight);
 }
 
 function draw() {
+	noStroke();
 	background(255, 255, 0)
+	ellipse(dot.PVector.x, dot.PVector.y, 50, 50).fill('black')
 }
