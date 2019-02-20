35c8db06a5f89a90fa8a56e058a081a94894216f
Peter <peter.cresswell7@gmail.com>
2019-02-20 14:37:45 +0000

    Changed mutation to have gene modification as a seperate function

--- Algorithm/Mutation.js
+++ Algorithm/Mutation.js
@@ -8,16 +8,26 @@ Mutation functions, collection of different mutation styles
 function Mutation(NewDots) {
     for (let dot of NewDots) {
         //For every dna vector
-        for (let dnaVector of dot.Brain.dna) {
+        for (let geneIndex in dot.Brain.dna) {
             //if random value is lower than mutation rate, mutate the dna
             let rand = Math.random();
             if (rand < Settings.mutationRate) {
-                //adds half a random vector (half to reduce mutation impact.)
-                let randVector = p5.Vector.random2D();
-                let halfRandVector = createVector(randVector.x / 2, randVector.x / 2);
-                dnaVector.add(halfRandVector);
+                dot.Brain.dna[geneIndex] = GeneModification(dot.Brain.dna[geneIndex], 1, 0.5);
             }
         }
     }
     return NewDots;
+}
+
+function GeneModification(gene, currentMultiplyer, mutationMultiplyer) {
+    let mutation = p5.Vector.random2D();
+    mutation.x = mutation.x * mutationMultiplyer;
+    mutation.y = mutation.y * mutationMultiplyer;
+
+    let currentGene = gene.copy();
+    currentGene.x = currentGene.x * currentMultiplyer;
+    currentGene.y = currentGene.y * currentMultiplyer;
+
+    mutation.add(currentGene);
+    return mutation;
 }
